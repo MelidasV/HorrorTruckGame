3dd4e91cebdb58b2c9b6ea9a08d96b8314208851
Victor Ramos <jsvramos@live.com>
2026-01-22 08:23:01 -0300

    Modificação nos script de valor do item

---
+++ CaminhaoGame/Assets/Script/itemOLD.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ItemOld : MonoBehaviour
+{
+    public ItemData data;
+    public bool sendoSegurado = false;
+
+    void FixedUpdate()
+    {
+        // Se for um item maldito, ele flutua levemente para assustar
+        if (data != null && data.ehMaldito && !sendoSegurado)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * Mathf.Sin(Time.time * 2f) * 5f);
+        }
+    }
+}
